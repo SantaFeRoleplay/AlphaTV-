f1306c136292f7b6d40d338e50b42ff8f33f44f7
AlphaTV <56059997+SantaFeRoleplay@users.noreply.github.com>
2026-09-15 05:16:29 -0300

    Refactor firebase-config.js to export firebaseConfig
    Removed Firebase initialization and exported the configuration.

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,3 @@
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAzHOtt5dLWIpZ3CReWE99gnvvzPjNTjDE",
   authDomain: "alphatv-ad6c6.firebaseapp.com",
@@ -17,7 +8,4 @@ const firebaseConfig = {
   appId: "1:835760614107:web:16827f9a7ea082bb56fafe",
   measurementId: "G-4N15KDFT74"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export { firebaseConfig };
